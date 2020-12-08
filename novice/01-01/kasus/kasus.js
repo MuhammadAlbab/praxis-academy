0f8dc5b0a78c9b704e129292927a15ff58c874ff
=MuhammadAlbab <=muhrizkialbab@gmail.com>
2020-12-08 16:24:19 +0700

    kasus 01-01

---
+++ novice/01-01/kasus/kasus.js
@@ -0,0 +1,32 @@
+var a = 20
+var b = 3
+var c = a + b
+var d = a * b
+var e = a - b
+var f = a / b
+var g = a % b
+var l = a**b
+var h = a > b
+var i = a < b
+var j = a === b
+var k = a !== b
+
+const phy = 3.14
+var r = 7
+var m = phy * r**2
+
+console.log("a =",a)
+console.log("b =",b)
+console.log("a + b :",c)
+console.log("a * b ;",d)
+console.log("a - b ;",e)
+console.log("a / b :",f)
+console.log("a % b :",g)
+console.log("a pangkat b :",l)
+console.log("a > b :",h)
+console.log("a < b :",i)
+console.log("nilai a sama dengan b: ",j)
+console.log("nilai a tidak sama dengan: ",k)
+console.log("jari-jari lingkaran: ",r)
+console.log("luas lingkaran (BELUM DIBULATKAN): ",m)
+console.log("luas lingkaran: ",Math.round(m))
