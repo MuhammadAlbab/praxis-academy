24de36910ce04031ac8d1809ac1b9d4088bef714
MuhammadAlbab <muhrizkialbab@gmail.com>
2020-12-23 16:54:45 +0700

    first-vue

---
+++ novice/03-02/latihan.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="latihan-1">
+        <h2>Hi Guys!</h2>
+        <h1>{{countKu}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'latihan',
+    props: {
+        countKu: Number
+    }
+}
+</script>
